5b6a00f4225a68f639c1c1727c938299ea27f086
kevmo <k.james.moore@gmail.com>
2015-07-27 08:37:25 -0500

    WIP

--- bounce.py
+++ bounce.py
@@ -10,5 +10,22 @@ tk.wm_attributes("-topmost", 1)
 canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
 canvas.pack()
 tk.update()
-tk.mainloop()
+print "BEFO"
+
+print "MAINLOOPIN"
+#models
+class Ball:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
+        self.canvas.move(self.id, 245, 100)
+
+    def draw(self):
+        pass
+
+if __name__ == '__main__':
+    ball = Ball(canvas, 'red')
+    tk.mainloop()
+    pass
+
 
