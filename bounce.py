9e5ae4d08a25954dc0565278c9692a3a679fd771
kevmo <k.james.moore@gmail.com>
2015-08-02 16:27:46 -0500

    Ball bouncin around

--- bounce.py
+++ bounce.py
@@ -2,30 +2,59 @@ from Tkinter import *
 import random
 import time
 
-# configure tk instance, prep canvas, open
+
 tk = Tk()
+
 tk.title("Bounce!")
 tk.resizable(0, 0)
 tk.wm_attributes("-topmost", 1)
-canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
-canvas.pack()
-tk.update()
-print "BEFO"
 
-print "MAINLOOPIN"
+# game board
+canvas = Canvas(tk, width=500, height=400,
+                bd=0, highlightthickness=0) # no border
+canvas.pack() # size yourself according to previous line
+tk.update() # fire ze missile
+
+
+
 #models
 class Ball:
     def __init__(self, canvas, color):
         self.canvas = canvas
-        self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
+        # identifier for this ball
+        self.id = canvas.create_oval(10, 10, 25, 25,
+                                     fill=color)
         self.canvas.move(self.id, 245, 100)
 
+        # positioning
+        starts = range(-3, 4)
+        random.shuffle(starts)
+        self.x = starts[0]
+        self.y = -3
+        self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width()
+
     def draw(self):
-        pass
+        self.canvas.move(self.id, self.x, self.y)
+        pos = self.canvas.coords(self.id)
+        print "POS:", pos
+
+        if pos[1] < 0:
+            self.y = 3
+        if pos[3] >= self.canvas_height:
+            self.y = -3
+        if pos[0] <=0:
+            self.x = 3
+        if pos[2] >= self.canvas_width:
+            self.x = -3
+
 
 if __name__ == '__main__':
     ball = Ball(canvas, 'red')
-    tk.mainloop()
+    # tk.mainloop()
+    while 1:
+        ball.draw()
+        tk.update_idletasks()
+        tk.update()
+        time.sleep(0.01)
     pass
-
-
